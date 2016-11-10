2f65a621f25e9f068cec8df53a275f1871fa6a82
RevolCat <skyestzhang@163.com>
2016-11-10 08:56:00 +0800

    1110

---
+++ config.js
@@ -0,0 +1,15 @@
+var mysql=require('mysql');
+var debug=require('debug')('blog:update');
+
+//创建数据连接
+var db=mysql.createConnection({
+    host:'127.0.0.1',
+    port:3306,
+    database:'blogcrawler',
+    user:'root',
+    password:'123456'
+});
+
+exports.sinaBlog={
+    url:'http://blog.sina.com.cn/liuxu28'
+};
